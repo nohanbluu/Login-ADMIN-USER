07ab54e869fd6fe146fe422ae11b46f09e6216f8
Noor Ahmad Naufal <nohanbluu@gmail.com>
2026-05-12 23:12:10 +0800

    Delete Login ADMIN-USER/docker-entrypoint.sh

--- Login ADMIN-USER/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Fix MPM setiap kali container start
-a2dismod mpm_event mpm_worker 2>/dev/null || true
-a2enmod mpm_prefork 2>/dev/null || true
-
-# Jalankan command aslinya (apache2-foreground)
-exec "$@"
